27456ded2a08e324f2933d85eef7492fd6b2133a
Sahil <slymishra@gmail.com>
2016-09-26 10:50:31 -0400

    adding some more infastructure

---
+++ support/Rect.java
@@ -0,0 +1,12 @@
+
+public class Rect {
+
+	public double X,Y,Width,Height;
+	
+	public Rect(double X,double Y, double Width,double Height){
+		this.X = X;
+		this.Y = Y;
+		this.Height = Height;
+		this.Width = Width;
+	}
+}
